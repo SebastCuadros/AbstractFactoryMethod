38207705beb649ce93a69b1feec77e2315b875dc
Juan Sebastián Gutiérrez Cuadros <sebastiangutierrezcuadros@gmail.com>
2026-02-13 22:38:57 -0500

    Se agrega función de seleccionar los elementos de cada menú

--- src/Main.java
+++ src/Main.java
@@ -44,18 +44,89 @@ public class Main {
                     continue;
             }
 
-            Entrada entrada = menu.crearEntrada();
-            Plato plato = menu.crearPlato();
-            Bebida bebida = menu.crearBebida();
-            Postre postre = menu.crearPostre();
-            
+            int a = 1;
+            while (a == 1) {
+                System.out.println("Desea pedir entrada?:");
+                System.out.println("(1)Si    (2)No: ");
+                a = leer.nextInt();
+
+                if (a == 1) {
+                    Entrada entrada = menu.crearEntrada();
+                    entrada.preparar();
+                    System.out.println("");
+                    a = 0;
+
+                } else if (a == 2) {
+                    break;
+                } else {
+                    System.out.println("Digite una opción válida");
+                    a = 1;
+                }
+            }
+
+            int b = 1;
+            while (b == 1) {
+                System.out.println("Desea pedir plato fuerte?:");
+                System.out.println("(1)Si    (2)No: ");
+                b = leer.nextInt();
+
+                if (b == 1) {
+                    Plato plato = menu.crearPlato();
+                    plato.preparar();
+                    System.out.println("");
+                    b = 0;
+
+                } else if (b == 2) {
+                    break;
+                } else {
+                    System.out.println("Digite una opción válida");
+                    b = 1;
+                }
+            }
+
+            int c = 1;
+            while (c == 1) {
+                System.out.println("Desea pedir bebida?:");
+                System.out.println("(1)Si    (2)No: ");
+                c = leer.nextInt();
+
+                if (c == 1) {
+                    Bebida bebida = menu.crearBebida();
+                    bebida.preparar();
+                    System.out.println("");
+                    c = 0;
+
+                } else if (c == 2) {
+                    break;
+                } else {
+                    System.out.println("Digite una opción válida");
+                    c = 1;
+                }
+            }
+
+            int d = 1;
+            while (d == 1) {
+                System.out.println("Desea pedir postre?:");
+                System.out.println("(1)Si    (2)No: ");
+                d = leer.nextInt();
+
+                if (d == 1) {
+                    Postre postre = menu.crearPostre();
+                    postre.preparar();
+                    System.out.println("");
+                    d = 0;
+
+                } else if (d == 2) {
+                    break;
+                } else {
+                    System.out.println("Digite una opción válida");
+                    d = 1;
+                }
+            }
+
             System.out.println("\n");
-            
-            entrada.preparar();
-            plato.preparar();
-            bebida.preparar();
-            postre.preparar();
-            
+            System.out.println("Orden creada con exito!");
+            System.out.println("\n");
 
             System.out.println("Desea ordenar otro plato?");
             System.out.println("(1)Si    (2)No: ");
